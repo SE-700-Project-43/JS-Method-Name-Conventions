5505320372c78361d2d4dce4c8dfcecd7c96c3b5
jfen445 <jfen445@aucklanduni.ac.nz>
2023-06-22 18:53:42 +1200

    Grammar check package implemented

--- splitter.py
+++ splitter.py
@@ -2,6 +2,15 @@ import csv
 from spiral import ronin
 import enchant
 import spacy
+import language_tool_python
+
+tool = language_tool_python.LanguageTool('en-US')
+
+# text = "the quick brown fox jumped the over chair"
+# matches = tool.check(text)
+# print("working " + text)
+# print(matches)
+
 
 d = enchant.Dict("en_US")
 nlp = spacy.load("en_core_web_sm")
@@ -20,18 +29,33 @@ with open('names.csv') as csv_file:
 
 print(variableNames)
 
-columnNames = ["Variable Name", "word", "isDictionary", "POS Tag", "Length"]
+columnNames = ["Variable Name", "word", "isDictionary", "POS Tag", "Length", "Grammar"]
 writer.writerow(columnNames)
+
+def all_lower(my_list):
+    return [x.lower() for x in my_list]
+
+def checkGrammar(name):
+    lowercase = all_lower(name)
+    sentence = " ".join(lowercase).capitalize()
+    matches = tool.check(sentence)
+
+    if not matches:
+        return True
+    else:
+        return False
+
 
 # analysis
 for s in variableNames:
     splitWords = ronin.split(s)
-
+    isGrammarCorrect = checkGrammar(splitWords)
     for word in splitWords:
         dictWord = d.check(word)
         # print(f)
         doc = nlp(word)
-        row = [s, word, str(dictWord), str(doc[0].pos_), len(word)]
+        row = [s, word, str(dictWord), str(doc[0].pos_), len(word), str(isGrammarCorrect)]
         writer.writerow(row)
 
 f.close()
+
