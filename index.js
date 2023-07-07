46776ae774944423bcf462fe5faafbb796c5b34b
Matthew <mlai962@aucklanduni.ac.nz>
2023-07-07 12:57:43 +1200

    added csv output file for total count of method name lengths in a repo

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ getFileList("./test_scripts")
   .then((res) => {
     let x = [];
     let y = [];
+    const nameLengthCounts = {};
+
     res.map(async (file, index) => {
       // console.log(file);
       await generateMethodNames(file).then((data) => {
@@ -20,10 +22,21 @@ getFileList("./test_scripts")
           for (let i = 0; i < data[2].length; i++) {
             fs.appendFileSync("./names.csv", data[2][i].toString());
             fs.appendFileSync("./names.csv", ",");
+            fs.appendFileSync(
+              "./names.csv",
+              data[2][i].toString().length.toString()
+            );
+            fs.appendFileSync("./names.csv", ",");
             fs.appendFileSync("./names.csv", file.toString());
             fs.appendFileSync("./names.csv", ",");
             fs.appendFileSync("./names.csv", data[3][i].toString());
             fs.appendFileSync("./names.csv", "\n");
+
+            nameLengthCounts[data[2][i].toString().length] = nameLengthCounts[
+              data[2][i].toString().length
+            ]
+              ? nameLengthCounts[data[2][i].toString().length] + 1
+              : 1;
           }
         }
 
@@ -33,6 +46,24 @@ getFileList("./test_scripts")
             fs.appendFileSync("./test.csv", ",");
             fs.appendFileSync("./test.csv", y[i].toString());
             fs.appendFileSync("./test.csv", "\n");
+          }
+
+          console.log(nameLengthCounts);
+          // nameLengthCounts.foreach((k, v) => {
+          //   // fs.appendFileSync()
+          // });
+          for (let length in nameLengthCounts) {
+            let count = nameLengthCounts[length];
+            fs.appendFileSync(
+              "./results_method_name_length_counts.csv",
+              length.toString()
+            );
+            fs.appendFileSync("./results_method_name_length_counts.csv", ",");
+            fs.appendFileSync(
+              "./results_method_name_length_counts.csv",
+              count.toString()
+            );
+            fs.appendFileSync("./results_method_name_length_counts.csv", "\n");
           }
         }
       });
