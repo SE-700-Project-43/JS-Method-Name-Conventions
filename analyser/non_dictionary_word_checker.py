f39a883b1cbc6cd35dba16b6a6b8c2e31a5a854b
jfen445 <jfen445@aucklanduni.ac.nz>
2023-08-19 18:41:32 +1200

    Added list for abbreviations and accronyms

--- analyser/non_dictionary_word_checker.py
+++ analyser/non_dictionary_word_checker.py
@@ -1,11 +1,28 @@
 abbrevations = []
-with open("abbreviation.txt", "r") as file:
+acronyms = []
+
+with open("./analyser/abbreviation.txt", "r") as file:
   for line in file:
     abbrevations.append(line.strip()) 
 
-print(abbrevations)
 
-with open("nonDictionary.txt", "r") as file:
+with open("./analyser/acronym.txt", "r") as file:
+  for line in file:
+    acronyms.append(line.strip()) 
+
+acronym_count = 0
+abbrevation_count = 0
+total_count = 0
+
+with open("./analyser/nonDictionary.txt", "r") as file:
   for word in file:
-    print(word, word in abbrevations)
+    total_count += 1
+    if word in abbrevations:
+      abbrevation_count += 1
+    if word in acronyms:
+      acronym_count += 1
+
+print("Abbreviation Percentage ", abbrevation_count/total_count * 100, "%")
+print("Acronyn Percentage :", acronym_count/total_count * 100, "%")
+
   
