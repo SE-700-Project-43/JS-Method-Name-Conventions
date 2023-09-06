9e03df41dfefe563c57f478a6f41b96a2448ccbd
Matthew <mlai962@aucklanduni.ac.nz>
2023-09-06 12:19:36 +1200

    implemented getting contributor age

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
 const readline = require("readline");
-
+const fetch = require("node-fetch");
 const shell = require("shelljs");
+require("dotenv").config();
 
 const path = process.cwd();
 
@@ -9,6 +10,40 @@ const path = process.cwd();
 function cloneRepo(repoLink) {
   shell.cd(path + "/parser/test_scripts");
   shell.exec(`git clone ${repoLink}`);
+}
+
+async function getContributors(url, token) {
+  const usernames = [];
+
+  await fetch(url, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      data.map((user) => {
+        usernames.push(user.login);
+      });
+    });
+
+  return usernames;
+}
+
+async function getUserCreatedAt(username, token) {
+  date = "";
+
+  await fetch(`https://api.github.com/users/${username}`, {
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      date = data.created_at;
+    });
+
+  return date;
 }
 
 //read in file containing JavaScript Github repositories
@@ -22,8 +57,34 @@ async function processLineByLine() {
 
   for await (const line of rl) {
     const splitName = line.split("/");
+    const ownerName = splitName[splitName.length - 2];
     const gitRepoName = splitName[splitName.length - 1];
     const repoName = gitRepoName.substring(0, gitRepoName.length - 4);
+
+    username = "mlai962";
+    password = process.env.GITHUB_TOKEN;
+
+    usernames = await getContributors(
+      `https://api.github.com/repos/${ownerName}/${gitRepoName.replace(
+        ".git",
+        ""
+      )}/contributors`,
+      password
+    );
+
+    const getAges = (usernames) => {
+      const promises = usernames.map(async (username) => {
+        return await getUserCreatedAt(username, password);
+      });
+      return Promise.all(promises);
+    };
+
+    dates = await getAges(usernames).then((data) => data);
+
+    ages = dates.map((date) => {
+      year = date.split("-")[0];
+      return 2023 - year;
+    });
 
     cloneRepo(line);
 
